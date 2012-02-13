e79c2e17b90acd498799f48a573e6d6df1fa10b8
jhawkins <josh.r.hawk@gmail.com>
2012-02-12 19:04:00 -0500

    more notes

--- Main.scala
+++ Main.scala
@@ -3,6 +3,7 @@ import player.Player
 object Main {
   def main(args: Array[String]) {
     for (arg <- args)
-      println(new Player(arg,1,1))
+      println(Player start arg)
+      Player count
   }
 }
--- Player.scala
+++ Player.scala
@@ -1,6 +1,33 @@
 package player
 
+// companion obj
+object Player {
+  import scala.collection.mutable.Map
+  
+  val players = Map[String, Player]()
+
+  // returns unit if no =
+  def count { println(players.size) }
+  
+  def start(name: String) = {
+    if (players contains name)
+      players(name)
+    else {
+      val p = new Player(name)
+      players += (name -> p)
+      p
+    }
+  }
+}
+
 class Player(name: String, x:Int, y:Int) {
+
+  // condition on init
   require(name != "")
-  override def toString = name + " @ x: " + x + " y: " + y 
+  
+  // second constructor
+  def this(name: String) = this(name, 1, 1)
+  
+  // override example
+  override def toString = name + " @ x: " + x + " y: " + y
 }
